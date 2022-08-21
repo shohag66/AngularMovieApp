6c8467c59abe3481f19985d6167ed5e529590fc5
shohag66 <professional.cse66@gmail.com>
2022-08-21 09:24:19 +0600

    CreateComponenty&Service

--- AngularMovieApp/src/app/app.module.ts
+++ AngularMovieApp/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import{HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { DashbordComponent } from './Component/dashbord/dashbord.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashbordComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
